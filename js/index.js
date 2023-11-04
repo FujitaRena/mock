7036a6ca42ced4f923e13d1508a4679876ee335f
FujitaRena <renafujita0925@icloud.com>
2023-11-04 23:51:21 +0900

    ファイル選択、送信ボタン押下時の処理追加

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function submit_msg() {
+    const ta1 = document.getElementById('message').value;
+    document.getElementById('mymessage').textContent = ta1;
+
+    const now = new Date();
+    const nowYear = now.getFullYear(); 
+    const nowMon = ("0" + (now.getMonth()+1)).slice(-2);
+    const nowDate = ("0" + now.getDate()).slice(-2);
+    const nowHour = ("0" + now.getHours()).slice(-2);
+    const nowMinute = ("0" + now.getMinutes()).slice(-2);
+    document.getElementById('date').innerText = nowYear + '/' + nowMon + '/' + nowDate + ' ' + nowHour  + ':' + nowMinute;
+}
+
+
+
+window.onload = function() {
+
+    const upload = document.getElementById('upload_file');
+    upload.addEventListener('click',async () => {
+        const fh_list = await window.showOpenFilePicker();
+        const fh = fh_list[0];
+        const file = await fh.getFile();
+        const reader = new FileReader();
+        reader.readAsText(file);
+        file_name.innerText = fh_list[0].name;
+    });
+}
